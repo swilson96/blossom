95ab7456dd1e29194df9a3bd419ac4b057bacc39
Simon Wilson <sjw@softwire.com>
2016-08-28 17:15:24 +0100

    ugly but functional results display

--- app/displayResults.js
+++ app/displayResults.js
@@ -7,12 +7,10 @@ class DisplayResults extends React.Component {
   render() {
     return (
       <div className="results">
-        <h2>RESULTS</h2>
+        {this.props.result.length? <h2>RESULTS</h2> : ''}
         {this.props.result.map(pair => {
             return(
-              <div key={pair[0]}>
-                <span>{pair[0]}</span>&nbsp;<span>{pair[1]}</span>
-              </div>
+              <div key={pair[0]}>{pair[0]}, {pair[1]}</div>
             )
         })}
       </div>
