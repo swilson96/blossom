9e55f6ef2220c5e7410ea5646928c1b3b175f6fc
Simon Wilson <sjw@softwire.com>
2016-09-11 11:20:29 +0100

    Updating signature of basic test matching algorithm

--- algorithm/index.js
+++ algorithm/index.js
@@ -15,7 +15,7 @@ export const firstMatching = (nodes, edges) => {
   var previous;
   nodes.forEach( node => {
     if (previous) {
-      pairs.push([previous.name, node.name]);
+      pairs.push([previous, node]);
       previous = null;
     } else {
       previous = node;
